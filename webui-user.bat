0ea0743bcd4159d50e314bda60a950c4818ca968
bencoster <ben@mycyberisland.com>
2022-11-19 23:31:53 +1100

    Update webui-user.bat
    Try launching Windows Edge to http://127.0.0.1:7860 after starting SD webservice

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--xformers 
+git pull
 call webui.bat
+cmd /K "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory=Default  http://127.0.0.1:7860
+exit
